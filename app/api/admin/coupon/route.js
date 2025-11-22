2dafbed568ff841763959664769a95597b362c39
peter thuku <peterthuku706@gmail.com>
2025-11-22 23:49:25 +0300

    trigger

--- app/api/admin/coupon/route.js
+++ app/api/admin/coupon/route.js
@@ -1,3 +1,4 @@
+import { inngest } from "@/inngest/client";
 import prisma from "@/lib/prisma";
 import authAdmin from "@/middlewares/authAdmin";
 import { getAuth } from "@clerk/nextjs/server";
@@ -15,7 +16,16 @@ export async function POST(request) {
     const { coupon } = await request.json();
     coupon.code = coupon.code.toUpperCase();
 
-    await prisma.coupon.create({ data: coupon });
+    await prisma.coupon.create({ data: coupon }).then(async (coupon) => {
+        await inngest.send({
+            name: "app/coupon.expired",
+            data: {
+                code: coupon.code,
+                expires_at: new Date(coupon.expiresAt)
+            }
+        })
+    })
+
     return NextResponse.json({ message: "Coupon added successfully" });
   } catch (error) {
     console.error(error);
